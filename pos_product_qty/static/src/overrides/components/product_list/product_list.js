1b83d4a318e5f67a29599462a096a1ab26779f3d
Leonardo <leml@odoo.com>
2024-02-22 14:56:28 -0600

    [ADD]removed pycache and vscode files, added gitignore and fixed bug

--- pos_product_qty/static/src/overrides/components/product_list/product_list.js
+++ pos_product_qty/static/src/overrides/components/product_list/product_list.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
+import { ConnectionLostError } from "@web/core/network/rpc_service";
 import { ProductsWidget } from "@point_of_sale/app/screens/product_screen/product_list/product_list";
 import { onMounted, onWillUnmount } from "@odoo/owl"
 
@@ -23,13 +24,15 @@ patch(ProductsWidget.prototype, {
 
     async updateQtyOnHand(productsList) {
         const productIds = productsList.map((p) => p.id)
-        this.pos.set_synch("connecting", productIds.length)
+        this.pos.set_synch('connecting', productIds.length)
         try {
             const result = await this.orm.silent.call('product.product', 'get_products_qty', [productIds, this.pos.config.warehouse_id])
             this.pos.set_synch('connected')
             productsList.forEach((p) => p.available_quantity = result[p.id])
-        } catch(ConnectionError) {
-            this.pos.set_synch('disconnected')
+        } catch(error) {
+            if (error instanceof ConnectionLostError) {
+                this.pos.set_synch('disconnected')
+            }
         }
     }
 })
